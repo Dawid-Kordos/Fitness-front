a076578b36e695115e0096a40f6271a586079784
Dawid Kordos <dkordos82@wp.pl>
2022-07-03 19:38:33 +0200

    Added types for implementing new trainings.

---
+++ src/utils/activityDefaultData.ts
@@ -0,0 +1,9 @@
+export const activityDefaultData = {
+    activityName: '',
+        activityDate: '',
+        activityStartTime: '',
+        activityDuration: 0,
+        activityDistance: 0,
+        activitySpeed: 0,
+        activityComment: '',
+};
---
+++ src/utils/activityTypes.ts
@@ -0,0 +1,12 @@
+import {ActivityTypeEntity} from "../types/ActivityTypeEntity";
+
+export const activityTypes: ActivityTypeEntity[] = [
+    {
+        id: '1',
+        name: 'running',
+    },
+    {
+        id: '2',
+        name: 'swimming',
+    }
+];
