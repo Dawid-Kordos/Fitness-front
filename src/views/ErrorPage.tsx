6cf6a7a223ba71ba963b96cac028519573e1297e
Dawid Kordos <dkordos82@wp.pl>
2022-07-11 23:31:52 +0200

    Changed Link to Button component when error occur.

--- src/views/ErrorPage.tsx
+++ src/views/ErrorPage.tsx
@@ -2,17 +2,24 @@ import React from "react";
 import {Link} from "react-router-dom";
 
 import './ErrorPage.css';
+import {Button} from "../components/Button/Button";
 
 interface Props {
     message: string;
 }
 
-export const ErrorPage = (props: Props) => (
-    <>
-        <div className="Error">
-            <h2>There is an error:</h2>
-            {props.message}
-        </div>
-        <Link to='/' className="Error__link">Go to main page</Link>
-    </>
-);
+export const ErrorPage = (props: Props) => {
+    const handleButtonClick = () => {
+        window.location.reload();
+    }
+
+    return (
+        <>
+            <div className="Error">
+                <h2>There is an error:</h2>
+                {props.message}
+            </div>
+            <Button className='Error__btn' text='Try again' onClick={handleButtonClick}/>
+        </>
+    );
+}
